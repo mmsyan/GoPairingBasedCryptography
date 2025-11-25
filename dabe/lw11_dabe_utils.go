d2da091703a07dc567a9333a1628c5c439d68288
mmsyan <88571295+mmsyan@users.noreply.github.com>
2025-11-25 17:09:54 +0800

    feat: provide NewLW11DABEAttributesFromStrings func to convert attribute strings to attribute struct

--- dabe/lw11_dabe_utils.go
+++ dabe/lw11_dabe_utils.go
@@ -1,12 +1,28 @@
 package dabe
 
-import "github.com/consensys/gnark-crypto/ecc/bn254/fr"
+import (
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/mmsyan/GnarkPairingProject/hash"
+)
 
 // NewLW11DABEAttributes 创建一个新的属性集合
 // 返回指针，方便链式调用和避免大结构体拷贝
 func NewLW11DABEAttributes(attrs ...fr.Element) *LW11DABEAttributes {
 	copied := make([]fr.Element, len(attrs))
 	copy(copied, attrs)
+	return &LW11DABEAttributes{
+		attributes: copied,
+	}
+}
+
+// NewLW11DABEAttributesFromStrings 从字符串创建属性集合
+// 每个字符串会通过 hash.ToField 方法转换为 fr.Element
+// 返回指针，方便链式调用和避免大结构体拷贝
+func NewLW11DABEAttributesFromStrings(attrs ...string) *LW11DABEAttributes {
+	copied := make([]fr.Element, len(attrs))
+	for i, attr := range attrs {
+		copied[i] = hash.ToField(attr)
+	}
 	return &LW11DABEAttributes{
 		attributes: copied,
 	}
