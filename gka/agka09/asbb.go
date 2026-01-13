0b840e9a4cffddaf283230bf7f5c1e3eedcb57a7
mmsyan <88571295+mmsyan@users.noreply.github.com>
2026-01-13 15:08:02 +0800

    feat: implement struct definition in asbb.go

--- gka/agka09/asbb.go
+++ gka/agka09/asbb.go
@@ -1,21 +1,36 @@
 package agka09
 
+import (
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+)
+
 type PublicKey struct {
+	R bn254.G2Affine
+	A bn254.GT
 }
 
 type PrivateKey struct {
+	R fr.Element
+	X bn254.G1Affine
 }
 
 type Message struct {
+	S []byte
 }
 
 type Signature struct {
+	Sigma bn254.G1Affine
 }
 
 type PlainText struct {
+	M bn254.GT
 }
 
 type CipherText struct {
+	C1 bn254.G2Affine
+	C2 bn254.G2Affine
+	C3 bn254.GT
 }
 
 func KeyGen() {
