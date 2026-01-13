4ffa59296f7f7a4170b8d4971acb81e8eec3046b
mmsyan <88571295+mmsyan@users.noreply.github.com>
2026-01-13 14:55:05 +0800

    feat: initial project structure for Asymmetric Group Key Agreement (AGKA) implementation

---
+++ gka/agka09/asbb.go
@@ -0,0 +1,39 @@
+package agka09
+
+type PublicKey struct {
+}
+
+type PrivateKey struct {
+}
+
+type Message struct {
+}
+
+type Signature struct {
+}
+
+type PlainText struct {
+}
+
+type CipherText struct {
+}
+
+func KeyGen() {
+
+}
+
+func Sign() {
+
+}
+
+func Verify() {
+
+}
+
+func Encrypt() {
+
+}
+
+func Decrypt() {
+
+}
